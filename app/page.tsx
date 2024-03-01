e047e777f157cea84c75e6fe4271b0b1a808ed79
joaqwin <joacopm15@gmail.com>
2024-03-01 15:24:12 -0300

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>HOLA PROBANDO SI</h1>
+      <h1>HOLA PROBANDO por segunda vez</h1>
     </main>
   );
 }
