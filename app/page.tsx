694768b4d6b0f8470b6e7d2cdccf3e5b0712b898
joaqwin <joacopm15@gmail.com>
2024-03-01 17:44:33 -0300

    probando pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>HOLA PROBANDO por segunda vez asd</h1>
+      <a href="/home/dell/Escritorio/dev/test-nextjs/app/pages.tsx">asd</a>
     </main>
   );
 }
