acbf2da8569c0cfdc0262f3435a88b45863a1fef
joaqwin <joacopm15@gmail.com>
2024-03-03 13:08:22 -0300

    SWR usage

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,11 +1,34 @@
-import { Suspense } from "react"
+'use client'
+
 import Cards from "../components/Cards"
+import useSWR from "swr"
+
+const fetcher = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.json()
+    return data
+}
 
 export default async function Page(){
-    console.log('hola')
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const json = await res.json()
+    const {data, error} = useSWR('/posts', fetcher)
+
+    if(error) return <div>Failed to load</div>
+    if(!data) return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="rounded-full h-20 w-20 bg-violet-800 animate-ping"></div>
+        </div>
+    )
     return (
+        <section>
+            {data.map((item: any, index:any) => (
+                    <Cards key={index} title={item.title} body={item.body}></Cards>
+                )
+            )}
+        </section>
+    )
+}
+
+ /*return (
     <section>
         {json.map((item: any, index: any) => (
             <Cards key={index} title={item.title} body={item.body}></Cards>
@@ -13,5 +36,4 @@ export default async function Page(){
         }
     </section>    
         
-    );
-}
+    );*/
