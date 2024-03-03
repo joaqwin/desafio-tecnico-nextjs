b38aec266ac85a2a294a9f918976ae19adc9b0bb
joaqwin <joacopm15@gmail.com>
2024-03-03 11:12:51 -0300

    Solucionando problema con las props de Cards

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -1,4 +1,9 @@
-const Cards = props => {
+interface CardsProps {
+    title: string;
+    body: string;
+}
+
+const Cards: React.FC<CardsProps> = (props) => {
     return (
         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
