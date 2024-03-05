cb111456401a8e4f197db96a98f7bb7651b37058
joaqwin <joacopm15@gmail.com>
2024-03-05 15:56:05 -0300

    step 5 listo

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -15,9 +15,24 @@ const fetcher = async (url: string) => {
 export default function Page(){
     const [val, setVal] = useState("")
     const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/posts');
-    const [debounced] = useDebounce(val, 1000)
+    const [debounced] = useDebounce(val, 500)
 
-    const {data, error} = useSWR('/posts', () => fetcher(url))
+    const { data, error, isValidating} = useSWR('/posts', () => fetcher(url), {
+        onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+            // Never retry on 404.
+            if (error.status === 404) return <div>Failed to load</div>
+         
+            // Never retry for a specific key.
+            if (key === '/api/user') return
+         
+            // Only retry up to 10 times.
+            if (retryCount >= 2) return  alert('We are having some problems... Please refresh')
+         
+            // Retry after 5 seconds.
+            setTimeout(() => revalidate({ retryCount }), 5000)
+          }
+        })
+
     const { mutate } = useSWRConfig()
     
     const change = (event: any) => {
@@ -35,8 +50,9 @@ export default function Page(){
     }, [debounced])
 
     if(error) return <div>Failed to load</div>
-    if(!data) return (
+    if(isValidating) return (
         <div className="flex justify-center items-center h-screen">
+            <h1>Loading..........</h1>
             <div className="rounded-full h-20 w-20 bg-violet-800 animate-ping"></div>
         </div>
     )
